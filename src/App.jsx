9c6f19bd339f2d405b98c42c772c946edef3bb04
wookhyunKim <kwh1041@naver.com>
2024-11-05 20:30:55 +0900

    [#43] bug fix App.css path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
 import HomePage from './pages/HomePage.jsx';
 import NicknamePage from './pages/NicknamePage.jsx';
 import HostGuestPage from './pages/HostGuestPage.jsx';
-import HostPage from './pages/HostPage.jsx';
-import GuestPage from './pages/GuestPage.jsx';
 import GameRoomPage from './pages/GameRoomPage.jsx'
 import TakePhotos from './pages/TakePhotos.jsx';
 function App() {
@@ -15,10 +12,8 @@ function App() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/nickname" element={<NicknamePage/>} />
         <Route path="/hostguest" element={<HostGuestPage/>}/>
-        <Route path='/host' element={<HostPage/>}/>
-        <Route path='/guest' element={<GuestPage/>}/>
         <Route path='/gameroom' element={<GameRoomPage/>}/>
-        <Route path='/gameroom' element={<TakePhotos/>}/>
+        <Route path='/photo' element={<TakePhotos/>}/>
         {/* <Route path="/" element={<Test/>}/> */}
     </Routes>
 </BrowserRouter>
